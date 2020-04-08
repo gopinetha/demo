57658090e3435bc0d9a56e069e3ebf5121f65ba9
gopinetha <naranigopichand@gmail.com>
2020-04-08 16:52:49 +0530

    Adding anagram program in java

---
+++ Anagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Anagram
+{ 
+    public static String sortString(String Str) 
+    {
+        char tempArray[] = Str.toCharArray();
+        Arrays.sort(tempArray); 
+        return new String(tempArray); 
+    } 
+      
+   
+    public static void main(String[] args) 
+    { 
+	Scanner sc=new Scanner(System.in);
+        String Str1 = sc.next();
+		String Str2=sc.next();		
+        String out1 = sortString(Str1); 
+		 String out2 = sortString(Str2); 
+
+          if(out1.equals(out2)){
+        System.out.println("true");
+		}
+		else
+	System.out.println("false");
+    } 
+} 
