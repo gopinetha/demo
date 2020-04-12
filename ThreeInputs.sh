cec76d31974691b35d1c58983e997dca4b4c9f2c
gopinetha <naranigopichand@gmail.com>
2020-04-12 22:37:06 +0530

    reading 3 inputs from user

---
+++ ThreeInputs.sh
@@ -0,0 +1,9 @@
+
+
+#!/usr/bin/bash
+
+echo "Reading three inputs "
+read -p "Enter the first input variable : " a
+read -p "Enter the Second input : " b
+read -p "Enter the third input : " c
+echo $a "  " $b "  " $c
