3d579380b1c2d467ae4e21fa794cdbbc61155d5b
gopinetha <naranigopichand@gmail.com>
2020-04-13 12:42:48 +0530

    Storging Arthemitic results from Dictionary to Arrays

---
+++ Array.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+echo "Arthemetic operations and storing in dictionary"
+set -x
+
+read -p "Enter the first input : " a
+read -p "Enter the second Input : " b
+read -p "Enter the third input : " c
+d=$(( ($a+$b)*$c ))
+Dict["0"]="${d}"
+e=$(( $a*($b+$c) ))
+Dict["1"]="${e}"
+f=$(( ($a/$b)+$c ))
+Dict["2"]="${f}"
+g=$(( ($a%$b)+$c ))
+Dict["3"]="${g}"
+echo "result " ${Dict[@]}
+
+for ((i=0; i<4; i++))
+do
+arr[$i]=${Dict[$i]};
+done
+echo "result " ${arr[@]}
